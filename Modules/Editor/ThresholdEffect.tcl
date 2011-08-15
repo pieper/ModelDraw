adad4ef0831d3bf381838249610a786e33e4f80c
Steve Pieper <pieper@bwh.harvard.edu>
2011-08-15 18:07:58 -0400

    ENH: new threshold in segmented operation
    This allows the threshold to be applied within previously
    segmented regions.  For example, if user has defined several
    regions in one label map, a new label map can be made
    where the threshold is applied with in each region.  An
    application might be to use erode and save island to
    disconnect a region, and then dilate to include the full
    extent; then a threshold in segmented would restrore the
    nice boundaries without connection through the leak.

--- Modules/Editor/ThresholdEffect.tcl
+++ Modules/Editor/ThresholdEffect.tcl
@@ -44,7 +44,9 @@ if { [itcl::find class ThresholdEffect] == "" } {
     method previewOptions {} {}
     method setAnimationState { p } {}
     method applyOptions {} {}
+    method applyInSegmented {} {}
     method setPaintThreshold {} {}
+    method labelSelectDialog {} {}
   }
 }
 
@@ -196,12 +198,34 @@ itcl::body ThresholdEffect::buildOptions { } {
 
   pack [$o(range) GetWidgetName] -side top -anchor e -fill x -padx 2 -pady 2 
 
+  #
+  # an apply in segmented
+  #
+  set o(applyInSegmented) [vtkNew vtkKWPushButton]
+  $o(applyInSegmented) SetParent [$this getOptionsFrame]
+  $o(applyInSegmented) Create
+  $o(applyInSegmented) SetText "Apply In Segmented..."
+  $o(applyInSegmented) SetBalloonHelpString "Create a new label map where this threshold is applied to each label value of the current label map."
+  pack [$o(applyInSegmented) GetWidgetName] \
+    -side bottom -anchor e -padx 2 -pady 2 
+
+  #
+  # buttons frame
+  #
+  set o(buttonsFrame) [vtkNew vtkKWFrame]
+  $o(buttonsFrame) SetParent [$this getOptionsFrame]
+  $o(buttonsFrame) Create
+  $o(buttonsFrame) SetBorderWidth 0
+  $o(buttonsFrame) SetReliefToFlat
+  pack [$o(buttonsFrame) GetWidgetName] \
+    -side bottom -anchor e -padx 2 -pady 2 
+
 
   #
   # a cancel button
   #
   set o(cancel) [vtkNew vtkKWPushButton]
-  $o(cancel) SetParent [$this getOptionsFrame]
+  $o(cancel) SetParent $o(buttonsFrame)
   $o(cancel) Create
   $o(cancel) SetText "Cancel"
   $o(cancel) SetBalloonHelpString "Cancel threshold without applying to label map."
@@ -212,7 +236,7 @@ itcl::body ThresholdEffect::buildOptions { } {
   # use for painting button
   #
   set o(useForPainting) [vtkNew vtkKWPushButton]
-  $o(useForPainting) SetParent [$this getOptionsFrame]
+  $o(useForPainting) SetParent $o(buttonsFrame)
   $o(useForPainting) Create
   $o(useForPainting) SetText "Use For Paint"
   $o(useForPainting) SetBalloonHelpString "Transfer the current threshold settings to be used for Paint and Draw operations."
@@ -223,12 +247,13 @@ itcl::body ThresholdEffect::buildOptions { } {
   # an apply button
   #
   set o(apply) [vtkNew vtkKWPushButton]
-  $o(apply) SetParent [$this getOptionsFrame]
+  $o(apply) SetParent $o(buttonsFrame)
   $o(apply) Create
   $o(apply) SetText "Apply"
   $o(apply) SetBalloonHelpString "Apply current threshold settings to the label map."
   pack [$o(apply) GetWidgetName] \
     -side right -anchor e -padx 2 -pady 2 
+
 
   if { [$this getInputBackground] == "" || [$this getInputLabel] == "" } {
     $this errorDialog "Need to have background and label layers to use threshold"
@@ -253,13 +278,15 @@ itcl::body ThresholdEffect::buildOptions { } {
   lappend _observerRecords "$o(useForPainting) $tag"
   set tag [$o(cancel) AddObserver AnyEvent "after idle ::EffectSWidget::RemoveAll"]
   lappend _observerRecords "$o(cancel) $tag"
+  set tag [$o(applyInSegmented) AddObserver AnyEvent "$this labelSelectDialog"]
+  lappend _observerRecords "$o(applyInSegmented) $tag"
 
 }
 
 itcl::body ThresholdEffect::tearDownOptions { } {
   chain
   if { [info exists o(range)] } {
-    foreach w "range apply useForPainting cancel" {
+    foreach w "range buttonsFrame apply useForPainting cancel applyInSegmented" {
       if { [info exists o($w)] } {
         $o($w) SetParent ""
         pack forget [$o($w) GetWidgetName] 
@@ -287,4 +314,155 @@ itcl::body ThresholdEffect::setPaintThreshold {} {
 
   eval EditorSetPaintThreshold [$o(range) GetRange]
   EditorSetPaintThresholdState 1
+}
+
+itcl::body ThresholdEffect::applyInSegmented { } {
+  # create a new label map that has one thresholded region for each
+  # of the unique label values in the source label map
+  $this labelSelectDialog
+  set sourceLabels [$o(labelMapSelector) GetSelected]
+
+  #
+  # first, make the binary thresholded image (over the whole volume)
+  #
+  set thresh [vtkImageThreshold New]
+  $thresh SetInput [$this getInputBackground]
+  eval $thresh ThresholdBetween $range
+  $thresh SetInValue 1
+  $thresh SetOutValue 0
+  $thresh SetOutputScalarType [[$sourceLabels GetImageData] GetScalarType]
+  $thresh Update
+
+  #
+  # now, make a combinedImage that has the threhold masked
+  # for each of the distinct labels
+  #
+  set accum [vtkImageAccumulate New]
+  $accum SetInput [$sourceLabels GetImageData]
+  $accum Update
+  set lo [expr int([lindex [$accum GetMin] 0])]
+  set hi [expr int([lindex [$accum GetMax] 0])]
+  $accum Delete
+
+  set labelExtract [vtkImageThreshold New]
+  set logic [vtkImageLogic New]
+  $logic SetOperationToAnd
+  $logic SetOutputTrueValue 1
+  set changer [vtkImageLabelChange New]
+  $changer SetInput [$logic GetOutput]
+  set combiner [vtkImageLabelCombine New]
+  set combinedImage ""
+  for {set i $lo} {$i <= $hi} {incr i} {
+    if { $i == 0 } { 
+      # skip the background
+      continue 
+    }
+    $labelExtract SetInput [$sourceLabels GetImageData]
+    $labelExtract SetInValue 1
+    $labelExtract SetOutValue 0
+    $labelExtract ReplaceInOn
+    $labelExtract ReplaceOutOn
+    $labelExtract ThresholdBetween $i $i
+    $labelExtract SetOutputScalarType [[$sourceLabels GetImageData] GetScalarType]
+    $labelExtract Update
+    if { [[$labelExtract GetOutput] GetScalarRange] != " 0.0 0.0" } {
+      # AND of binary threshold and binary extracted label region
+      $logic SetInput1 [$thresh GetOutput]
+      $logic SetInput2 [$labelExtract GetOutput]
+      # convert result of AND back to label value
+      $changer SetInputLabel 1
+      $changer SetOutputLabel $i
+      $changer Update
+
+      # now combine it
+      if { $combinedImage == "" } {
+        set combinedImage [vtkImageData New]
+        $combinedImage DeepCopy [$changer GetOutput]
+      } else {
+        $combiner SetInput1 $combinedImage
+        $combiner SetInput2 [$changer GetOutput]
+        $combiner Update
+        $combinedImage DeepCopy [$combiner GetOutput]
+      }
+    }
+  }
+
+  # put the result in this label map
+  [$this getOutputLabel] DeepCopy $combinedImage
+  $this postApply
+
+  $labelExtract Delete
+  $thresh Delete
+  $changer Delete
+  $combiner Delete
+  $combinedImage Delete
+
+  after idle ::EffectSWidget::RemoveAll
+}
+
+# Label Select Dialog - prompt to pick a label map for applyInSegmented
+itcl::body ThresholdEffect::labelSelectDialog { } {
+
+  if { ![info exists o(labelSelectTopLevel)] } {
+    set o(labelSelectTopLevel) [vtkNew vtkKWTopLevel]
+    $o(labelSelectTopLevel) SetApplication $::slicer3::Application
+    $o(labelSelectTopLevel) ModalOn
+    $o(labelSelectTopLevel) Create
+    $o(labelSelectTopLevel) SetMasterWindow [$::slicer3::ApplicationGUI GetMainSlicerWindow]
+    $o(labelSelectTopLevel) SetDisplayPositionToPointer
+    $o(labelSelectTopLevel) HideDecorationOff
+    $o(labelSelectTopLevel) Withdraw
+    $o(labelSelectTopLevel) SetBorderWidth 2
+    $o(labelSelectTopLevel) SetReliefToGroove
+
+    set topFrame [vtkNew vtkKWFrame]
+    $topFrame SetParent $o(labelSelectTopLevel)
+    $topFrame Create
+    pack [$topFrame GetWidgetName] -side top -anchor w -padx 2 -pady 2 -fill both -expand true
+
+    set o(labelPromptLabel) [vtkNew vtkKWLabel]
+    $o(labelPromptLabel) SetParent $topFrame
+    $o(labelPromptLabel) Create
+    pack [$o(labelPromptLabel) GetWidgetName] -side top -anchor w -padx 2 -pady 2 -fill both -expand true
+
+    set o(labelMapSelector) [vtkNew vtkSlicerNodeSelectorWidget]
+    $o(labelMapSelector) SetParent $topFrame
+    $o(labelMapSelector) Create
+    $o(labelMapSelector) SetNodeClass "vtkMRMLScalarVolumeNode" "LabelMap" "1" ""
+    $o(labelMapSelector) NewNodeEnabledOff
+    $o(labelMapSelector) NoneEnabledOff
+    $o(labelMapSelector) DefaultEnabledOn
+    $o(labelMapSelector) ShowHiddenOn
+    $o(labelMapSelector) ChildClassesEnabledOff
+    $o(labelMapSelector) SetMRMLScene $::slicer3::MRMLScene
+    $o(labelMapSelector) UpdateMenu
+    $o(labelMapSelector) SetLabelText "Label Map:"
+    $o(labelMapSelector) SetBalloonHelpString "Pick the label map to use as the source for thresholding.\nNote that all non-zero label values will be used."
+    pack [$o(labelMapSelector) GetWidgetName] -side top -fill x -expand true
+
+    set buttonFrame [vtkNew vtkKWFrame]
+    $buttonFrame SetParent $topFrame
+    $buttonFrame Create
+    pack [$buttonFrame GetWidgetName] -side left -anchor w -padx 2 -pady 2 -fill both -expand true
+
+    set o(labelDialogApply) [vtkNew vtkKWPushButton]
+    $o(labelDialogApply) SetParent $buttonFrame
+    $o(labelDialogApply) Create
+    $o(labelDialogApply) SetText Apply
+    set o(labelDialogCancel) [vtkNew vtkKWPushButton]
+    $o(labelDialogCancel) SetParent $buttonFrame
+    $o(labelDialogCancel) Create
+    $o(labelDialogCancel) SetText Cancel
+    pack [$o(labelDialogCancel) GetWidgetName] [$o(labelDialogApply) GetWidgetName] -side left -padx 4 -anchor c 
+
+    # invoked event
+    set broker $::slicer3::Broker
+    $broker AddObservation $o(labelDialogApply) 10000 "$this applyInSegmented; $o(labelSelectTopLevel) Withdraw"
+    $broker AddObservation $o(labelDialogCancel) 10000 "$o(labelSelectTopLevel) Withdraw"
+  }
+
+  $o(labelPromptLabel) SetText "Select existing label map to define threshold regions."
+
+  $o(labelSelectTopLevel) DeIconify
+  $o(labelSelectTopLevel) Raise
 }
